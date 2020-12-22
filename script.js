d54de4151a6e0172ee3d6f95bf70c40892d5de62
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-12-21 22:06:43 -0500

    added basic layout for main modules

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// links game to appropriate dom elements for display
+const DOM = (function() {
+  return {
+    // functions and properties go in here
+
+  }
+})();
+
+// game board module
+const GameBoard = (function() {
+  return {
+    // functions and properties go in here
+
+  }
+})();
+
+// turn logic, win conditions and players
+const Game = (function () {
+  return {
+    // functions and properties go in here
+
+  }
+})();
