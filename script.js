d7c5bf1444d2d7285d99d0f8895cd4543f67f0c1
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-12-21 22:58:39 -0500

    added initial board generating functions

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 
-// links game to appropriate dom elements for display
 const DOM = (function() {
   return {
-    // functions and properties go in here
+    boardContainer : document.querySelector('.board'),
+    cells : document.querySelectorAll('.cell'),
+
+    updateBoard : function() {
+      GameBoard.getBoard().forEach((cell, idx) => {
+        this.cells[idx].innerHTML = cell.mark;
+      })
+    },
 
   }
 })();
 
-// game board module
 const GameBoard = (function() {
+  // private
+
+  // array containing actual board
+  const board = []
+
+  // cell factory function
+  const cell = function(mark) {
+    if(!mark) mark = '';
+    return {mark};
+  }
+  
   return {
-    // functions and properties go in here
+    
+    // returns board array
+    getBoard : () => { return board },
+    
+    // clears board and fills with 9 blank cells
+    newBoard : () => {
+      board.length = 0;
+      for (i = 0; i < 9; i++) {
+        board.push(cell(`${i}`));
+      }
+    },
+
 
   }
 })();
@@ -18,7 +45,6 @@ const GameBoard = (function() {
 // turn logic, win conditions and players
 const Game = (function () {
   return {
-    // functions and properties go in here
 
   }
 })();
