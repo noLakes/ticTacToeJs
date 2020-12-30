72cbb955bc2e4d6a9a1d32eb82d6e6bf48d373d9
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-12-29 20:19:47 -0500

    added win array

--- script.js
+++ script.js
@@ -71,6 +71,17 @@ const Game = (function () {
   // game logic variables
   let state = false;
   let turn = 1;
+
+  const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
   
   // player factory function
   const newPlayer = (name = 'Player', mark = 'X', human = true) => {
